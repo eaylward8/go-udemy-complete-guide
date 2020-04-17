4a1eeb834fbc0700a6a4593acc8b376554013180
Erik Aylward <eaylward@bdtrust.org>
2020-04-17 16:44:08 -0400

    3-26

--- cards/deck.go
+++ cards/deck.go
@@ -1,6 +1,10 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io/ioutil"
+	"strings"
+)
 
 // Create a new type "deck" which is a slice of strings
 type deck []string
@@ -13,21 +17,32 @@ func newDeck() deck {
 
 	for _, suit := range cardSuits {
 		for _, value := range cardValues {
-			cards = append(cards, value+" "+suit)
+			cards = append(cards, value+" of "+suit)
 		}
 	}
 
 	return cards
 }
 
+// takes a receiver of deck
 func (d deck) print() {
 	for i, card := range d {
 		fmt.Println(i, card)
 	}
 }
 
+// no receiver
 // takes args of deck and int
 // returns two decks
 func deal(d deck, handSize int) (deck, deck) {
 	return d[:handSize], d[handSize:]
+}
+
+func (d deck) toString() string {
+	return strings.Join([]string(d), ",")
+}
+
+// https://golang.org/pkg/io/ioutil/#WriteFile
+func (d deck) saveToFile(filename string) error {
+	return ioutil.WriteFile(filename, []byte(d.toString()), 0666)
 }
